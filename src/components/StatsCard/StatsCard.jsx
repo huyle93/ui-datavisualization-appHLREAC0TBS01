3d29b1ac574d2511d23187b164f037e83585f818
Huy Le <huyle.finance@gmail.com>
2018-03-31 22:21:41 -0400

    updated statsCard layout to 3:9, data, hover, shadow

--- src/components/StatsCard/StatsCard.jsx
+++ src/components/StatsCard/StatsCard.jsx
@@ -7,15 +7,17 @@ export class StatsCard extends Component{
             <div className="card card-stats">
                 <div className="content">
                     <Row>
-                        <Col xs={5}>
+                        <Col xs={3}>
                             <div className="icon-big text-center icon-warning">
                                 {this.props.bigIcon}
                             </div>
                         </Col>
-                        <Col xs={7}>
+                        <Col xs={9}>
                             <div className="numbers">
                                 <p>{this.props.statsText}</p>
+                                <h3>
                                 {this.props.statsValue}
+                                </h3>
                             </div>
                         </Col>
                     </Row>
