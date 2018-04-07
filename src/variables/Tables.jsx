6bcd76e64c7897bc114e86d6a8fa4accaec19393
Huy Le <huyle.finance@gmail.com>
2018-04-06 23:01:13 -0400

    updated data for table component, updated presentation layer. worked. no bugs.

--- src/variables/Tables.jsx
+++ src/variables/Tables.jsx
@@ -5,12 +5,15 @@
 //
 const thArray = ["Rank","Similar Attorney","Total Cases","Win Ratio","Recommended Division"];
 const tdArray = [
-    [ "1" , "Dakota Rice" , "36,738" , "87%" , "Oud-Turnhout" ] ,
-    [ "2" , "Minerva Hooper" , "23,789" , "84%" , "Sinaai-Waas" ] ,
-    [ "3" , "Sage Rodriguez" , "56,142" , "82%" , "Baileux" ] ,
-    [ "4" , "Philip Chaney" , "38,735" , "78%" , "Overland Park" ] ,
-    [ "5" , "Doris Greene" , "63,542" , "72%" , "Feldkirchen in Kärnten" ] ,
-    [ "6" , "Mason Porter" , "78,615" , "71%" , "Gloucester" ]
+    [ "1" , "Tony Bucknam" , "738" , "87%" , "Family" ] ,
+    [ "2" , "Mark Shellrot" , "389" , "84%" , "Criminal" ] ,
+    [ "3" , "Ben Lafferty" , "542" , "62%" , "Environmental" ] ,
+    [ "4" , "Melanie Kehne" , "835" , "78%" , "Civil" ] ,
+    [ "5" , "Melvin Fink" , "342" , "72%" , "Environmental" ] ,
+    [ "6" , "Rodney Dunkiel" , "615" , "71%" , "Civil" ],
+    [ "7" , "Benjamin Deppman" , "465" , "45%" , "Family" ],
+    [ "8" , "Ryan Readnour" , "223" , "82%" , "Civil" ]
+    
 ];
 
 module.exports = {
