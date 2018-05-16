0889a7d537ea58201d7d752ab9ce66f39227948a
Huy Le <huyle.finance@gmail.com>
2018-05-16 03:24:23 -0400

    Updated UserProfile /user route. Modified search component style

--- src/views/UserProfile/UserProfile.jsx
+++ src/views/UserProfile/UserProfile.jsx
@@ -11,8 +11,92 @@ import Button from 'elements/CustomButton/CustomButton.jsx';
 
 import avatar from "assets/img/faces/face-3.jpg";
 
+import Autosuggest from 'react-autosuggest';
+
+// Imagine you have a list of languages that you'd like to autosuggest.
+const languages = [
+    {
+        name: 'Thomas Durkin'
+    },
+    {
+        name: 'John Zaikowski'
+    },
+    {
+        name: 'Tony Adam'
+    },
+  ];
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+  
+    return inputLength === 0 ? [] : languages.filter(lang =>
+      lang.name.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  };
+  
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+    <div>
+        {suggestion.name}
+    </div>
+);
+
+
 class UserProfile extends Component {
+    constructor() {
+        super();
+
+        // Autosuggest is a controlled component.
+        // This means that you need to provide an input value
+        // and an onChange handler that updates this value (see below).
+        // Suggestions also need to be provided to the Autosuggest,
+        // and they are initially empty because the Autosuggest is closed.
+        this.state = {
+            value: '',
+            suggestions: []
+        };
+    }
+
+    onChange = (event, { newValue }) => {
+        this.setState({
+            value: newValue
+        });
+    };
+
+    // Autosuggest will call this function every time you need to update suggestions.
+    // You already implemented this logic above, so just use it.
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+            suggestions: getSuggestions(value)
+        });
+    };
+
+    // Autosuggest will call this function every time you need to clear suggestions.
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
+    // Test to see if click the page it would load
+    onSuggestionSelected = (event, { languages, suggestionValue, suggestionIndex, sectionIndex, method }) =>{
+    }
+
     render() {
+        const { value, suggestions } = this.state;
+
+        // Autosuggest will pass through all these props to the input.
+        const inputProps = {
+            placeholder: 'Search by Judge`s Name ',
+            value,
+            onChange: this.onChange,
+        };
         return (
             <div className="content">
                 <Grid fluid>
@@ -22,13 +106,28 @@ class UserProfile extends Component {
                                 content={
                                  <div className="name">
                                    <b><h1 style={{textAlign: "center", fontFamily: "Impact"}}>
-                                   Welcome Huy!</h1></b>
+                                   Welcome Sarah!</h1></b>
+                                   <div className="content" style={{textAlign: "center"}}>
+                                   <Autosuggest
+                                        suggestions={suggestions}
+                                        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                                        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                                        getSuggestionValue={getSuggestionValue}
+                                        onSuggestionSelected={this.onSuggestionSelected}  //. <-----
+                                        renderSuggestion={renderSuggestion}
+                                        inputProps={inputProps}
+                                        onSuggestionSelected={() => {
+                                            window.location.reload()
+                                            window.location.href = "http://localhost:3000/#/dashboard?Thomas-Durkin"
+                                        }}
+                                    />
+                                    </div>
                                 </div>
                                 }
                             />
                         </Col>
                         <Col md={12}>
-                            {/* <UserCard
+                            <UserCard
                                 bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
                                 avatar={avatar}
                                 name="Mike Andrew"
@@ -49,7 +148,7 @@ class UserProfile extends Component {
                                         <Button simple><i className="fa fa-google-plus-square"></i></Button>
                                     </div>
                                 }
-                            /> */}
+                            />
                         </Col>
                     </Row>
                 </Grid>
